6b4a0f87496cd68f487a9377d67692c6aba3bd49
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-03-31 18:27:11 -0300

    FuncionárioRegular

---
+++ FuncionarioRegular.java
@@ -0,0 +1,6 @@
+public class FuncionarioRegular extends Funcionario {
+    public FuncionarioRegular(String nome, double salarioPorHora, int horasTrabalhadasPorMes) {
+        super(nome, salarioPorHora, horasTrabalhadasPorMes);}
+    public double calcularSalarioMensal() {
+        return getSalarioPorHora() * getHorasTrabalhadasPorMes();}
+}
